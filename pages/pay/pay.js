8f63663080f2b9266cf1ac30cf2c8ee60eaf1d4a
“KANG” <2983973848@qq.com>
2021-10-20 19:32:45 +0800

    完成支付页面

--- pages/pay/pay.js
+++ pages/pay/pay.js
@@ -5,7 +5,92 @@ Page({
      * 页面的初始数据
      */
     data: {
+        address: {},
+        cart: [],
 
+        totalPrice: 0,
+        totalNumber: 0
+    },
+    editAddress() {
+        this.handleChooseAddress()
+    },
+    handleChooseAddress() {
+
+        //获取用户的权限状态
+        wx.getSetting({
+            success: (result) => {
+                const scopeAddress = result.authSetting["scope.address.address"]
+
+                if (scopeAddress === true || scopeAddress === undefined) {
+
+                    //获取收货地址
+                    wx.chooseAddress({
+                        success: (result2) => {
+                            //将收货地址信息保存在本地
+                            wx.setStorageSync('address', result2)
+                        },
+                    })
+
+
+                } else {
+
+                    //提示用户授权
+                    wx.openSetting({
+                        success: (result) => {
+                            //获取收货地址
+                            wx.chooseAddress({
+                                success: (result2) => {
+                                    //将收货地址信息保存在本地
+                                    wx.setStorageSync('address', result2)
+                                },
+                            })
+                        }
+                    })
+
+                }
+
+            }
+        })
+
+    },
+
+
+
+    handlePay() {
+
+
+
+
+    },
+
+    competePay() {
+        //获取缓存中的购物车数据
+        const cart = wx.getStorageSync('cart') || []
+
+        //过滤后的数组
+        let checkdCart = cart.filter(v => v.checked)
+
+
+
+        //计算购物车总价格和总数量
+        let totalPrice = 0;
+        let totalNumber = 0;
+
+        cart.forEach(v => {
+            if (v.checked) {
+                totalPrice += v.num * v.goods_price;
+                totalNumber += v.num
+            } else {
+
+            }
+
+        })
+
+        this.setData({
+            cart: checkdCart,
+            totalNumber,
+            totalPrice
+        })
     },
 
     /**
@@ -26,7 +111,19 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
+        //获取缓存中的收货地址信息
+        const address = wx.getStorageSync('address')
 
+        if (address) {
+            address.all = address.provinceName + address.cityName + address.countyName + address.detailInfo
+        }
+
+
+        this.setData({
+            address
+        })
+
+        this.competePay()
     },
 
     /**
